79081f7594678f7b12b0039c39bf092b29f8cb42
Anthony Korepanov <akorepanov@myseneca.ca>
2024-03-09 10:51:42 -0500

    Truck Struct Change

--- SourceCode/data.h
+++ SourceCode/data.h
@@ -21,6 +21,8 @@ struct PackageInf {
 };
 
 struct Truck {
+	int num_packages;
+	PackageInf* packages;
 	double m_totalSpace; //cubic meters. Cannot exceed BOX_SPACE
 	double m_totalWeight; // KG. Cannot exceed MAX_CARGO
 	char* route;          // assigned route
