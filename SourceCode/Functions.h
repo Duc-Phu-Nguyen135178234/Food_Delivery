d661a2b011373c88d4817df52719b42cc6711885
Duc-Phu-Nguyen135178234 <dpnguyen4@myseneca.ca>
2024-03-15 20:01:19 -0400

    Create Functions.h

---
+++ SourceCode/Functions.h
@@ -0,0 +1,23 @@
+
+#ifndef SENECA_DELIVERY_H
+#define SENECA_DELIVERY_H
+//header file for 6 function
+
+//Create function prototype getInput
+
+
+//Create function prototype check Packageweight
+
+
+//Create function prototype Boxsize
+
+//create function prototype Validdestination
+int Validdestination(const struct Map* routeMap, char* destination)
+// get char destination[] with size of 4 
+// check if correct format 8A 12A and map destination on routeMap return 1
+//if Destination out size the routeMap return 0
+
+
+
+//create function prototype check space of truck
+#endif //SENECA_DELIVERY_H
