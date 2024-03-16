d50c013be1ac098f557450fd5f2eb35b471ad3e4
Anthony Korepanov <akorepanov@myseneca.ca>
2024-03-16 13:38:47 -0400

    Configured Unit test with Extern C

--- SourceCode/UnitTest1/UnitTest1.cpp
+++ SourceCode/UnitTest1/UnitTest1.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "functions_r.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
---
+++ SourceCode/UnitTest1/functions_r.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef FUNCTIONS_R_H
+#define FUNCTIONS_R_H
+
+extern "C"
+{
+#include <Functions.h>
+#include <mapping.h>
+}
+#endif
