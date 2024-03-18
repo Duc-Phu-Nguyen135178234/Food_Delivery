4affa6e0e621b6acafd672f6b12896d12e7db346
- Jashandeep Singh <jashandeep-singh13@myseneca.ca>
2024-03-17 21:43:13 -0400

    Update Functions.h

--- SourceCode/Functions.h
+++ SourceCode/Functions.h
@@ -14,6 +14,15 @@ void getInput(struct packageInf* p);
 
 //Create function prototype Boxsize
 
+// Name
+int checkBoxSize(double shipmentSize);
+//description: The purpose of this function is to perform a thorough check and confirm whether the box size that is given meets the necessary criteria for being considered as valid or not.
+//parameters: shipmentSize
+//The function takes a parameter called ' shipmentSize ', which denotes the size of the box to be checked. To determine whether the given size is valid or not, the function defines three constant values - 'size1', 'size2', and 'size3' - that represent the acceptable box sizes. The function then compares the ' shipmentSize ' parameter with these constants.
+
+// returns
+//If the 'shipmentSize' matches any of the valid sizes, the function prints "valid" and returns 1 (true) to indicate that the box size is valid or “ invalid” and returns 0(false)  if it does not mach.
+
 //create function prototype Validdestination
 int Validdestination(const struct Map* routeMap, char* destination);
 // get char destination[] with size of 4 
