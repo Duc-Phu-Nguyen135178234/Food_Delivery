113f8d8a34c85b0314124c5aa0a218b9fadd0e30
Harmanpreet Singh <harmanpreetsingh@myseneca.ca>
2024-03-18 16:11:38 -0400

    done the work

--- SourceCode/UnitTest1/UnitTest1.cpp
+++ SourceCode/UnitTest1/UnitTest1.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "functions_r.h"
+#include <iostream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,6 +13,10 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
+			Point p1 = { 'a', '5' };
+			Point p2 = { 'b', '5' };
+			double a = distance(&p1, &p2);
+			Assert::AreEqual(1, a);
 		}
 	};
 }
--- SourceCode/main.c
+++ SourceCode/main.c
@@ -11,5 +11,6 @@ int main(void)
 
 	printMap(&routeMap, 1, 1);
 
+
 	return 0;
 }
