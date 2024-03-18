a0fa00578890070a86b421e8086d87bcccdbd9bf
Anthony Korepanov <akorepanov@myseneca.ca>
2024-03-18 19:37:09 -0400

    refactored project folders for unit test

--- SourceCode/UnitTest1/UnitTest1.cpp
+++ SourceCode/UnitTest1/UnitTest1.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "functions_r.h"
-#include <iostream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -13,10 +12,11 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
+			
 			Point p1 = { 'a', '5' };
 			Point p2 = { 'b', '5' };
 			double a = distance(&p1, &p2);
-			Assert::AreEqual(1, a);
+			Assert::AreEqual(1.0, a);
 		}
 	};
 }
--- SourceCode/UnitTest1/functions_r.h
+++ SourceCode/UnitTest1/functions_r.h
@@ -2,6 +2,8 @@
 #ifndef FUNCTIONS_R_H
 #define FUNCTIONS_R_H
 
+// C:\Users\Antho\OneDrive\Documents\winter24--sft221-naa-1\SourceCode\Project
+// C:\Users\Antho\OneDrive\Documents\winter24--sft221-naa-1\SourceCode\UnitTest1
 extern "C"
 {
 #include <Functions.h>
